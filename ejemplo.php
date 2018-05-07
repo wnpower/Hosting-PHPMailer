3f15853fbc7d4e2fabb116509e75034cce3eb1c9
imorandinwnp <imorandin@wnpower.com>
2018-05-07 17:36:34 -0300

    Corrección textos

--- ejemplo.php
+++ ejemplo.php
@@ -7,18 +7,17 @@ require 'PHPMailer/src/SMTP.php';
  
 $mail = new PHPMailer(true);
 try {
-    //Server settings
     $mail->SMTPDebug = 2;  // Sacar esta línea para no mostrar salida debug
     $mail->isSMTP();
     $mail->Host = 'mail.midominio.com';  // Host de conexión SMTP
     $mail->SMTPAuth = true;
-    $mail->Username = 'remitente@tudominio.com';                 // Usuario SMTP
+    $mail->Username = 'remitente@midominio.com';                 // Usuario SMTP
     $mail->Password = 'MiPa33W0rD';                           // Password SMTP
     $mail->SMTPSecure = 'tls';                            // Activar seguridad TLS
     $mail->Port = 587;                                    // Puerto SMTP
  
-    $mail->setFrom('remitente@tudominio.com');		// Mail del remitente
-    $mail->addAddress('destinatario@dominio.com');     // Mail del destinatario
+    $mail->setFrom('remitente@midominio.com');		// Mail del remitente
+    $mail->addAddress('destinatario@gmail.com');     // Mail del destinatario
  
     $mail->isHTML(true);
     $mail->Subject = 'Contacto desde formulario';  // Asunto del mensaje
