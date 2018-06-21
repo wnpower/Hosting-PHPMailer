5a94cc6db5e58e4fc81fe6adaa5e2807191cc595
imorandinwnp <imorandin@wnpower.com>
2018-06-21 12:39:44 -0300

    Habilitar excepciones

--- ejemplo.php
+++ ejemplo.php
@@ -1,7 +1,8 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
- 
+
+require 'PHPMailer/src/Exception.php';
 require 'PHPMailer/src/PHPMailer.php';
 require 'PHPMailer/src/SMTP.php';
  
